cdbf24cc2e23f9aba27e1b74d2023af07500f032
Te-afk4 <185973507+Te-afk4@users.noreply.github.com>
2024-11-05 00:00:00 +0500

    'add file utils.c'

---
+++ utils.c
@@ -0,0 +1,17 @@
+#include "utils.h"
+
+int check_winner(char board[3][3], char player) {
+    // Check rows and columns
+    for (int i = 0; i < 3; i++) {
+        if ((board[i][0] == player && board[i][1] == player && board[i][2] == player) ||
+            (board[0][i] == player && board[1][i] == player && board[2][i] == player)) {
+            return 1;
+        }
+    }
+    // Check diagonals
+    if ((board[0][0] == player && board[1][1] == player && board[2][2] == player) ||
+        (board[0][2] == player && board[1][1] == player && board[2][0] == player)) {
+        return 1;
+    }
+    return 0;
+}
