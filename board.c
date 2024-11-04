8ffcbe67bf4b0b316f30f673132a7c17ba8d626a
Te-afk4 <185973507+Te-afk4@users.noreply.github.com>
2024-11-05 00:00:00 +0500

    'add file board.c'

---
+++ board.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "board.h"
+
+void initialize_board(char board[3][3]) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            board[i][j] = ' ';
+        }
+    }
+}
+
+void print_board(char board[3][3]) {
+    printf("\n");
+    for (int i = 0; i < 3; i++) {
+        printf(" %c | %c | %c \n", board[i][0], board[i][1], board[i][2]);
+        if (i < 2) {
+            printf("---|---|---\n");
+        }
+    }
+    printf("\n");
+}
+
+int make_move(char board[3][3], int row, int col, char player) {
+    if (row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == ' ') {
+        board[row][col] = player;
+        return 1; // Move successful
+    }
+    return 0; // Move failed
+}
